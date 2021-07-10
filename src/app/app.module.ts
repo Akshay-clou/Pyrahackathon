a4a43f98b39a733bdd4e5f4b5246382a71183420
akshayPyraworkz <akshay.saini@pyraworkz.in>
2021-07-10 11:16:34 +0530

    all components created an routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ViewCartComponent } from './view-cart/view-cart.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { AddressComponent } from './address/address.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ViewCartComponent,
+    OrderSummaryComponent,
+    AddressComponent,
+    ProductDetailsComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
