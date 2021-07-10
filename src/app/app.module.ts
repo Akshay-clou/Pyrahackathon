8cc3326747888044263a77c668779a5a2a823a0f
akshayPyraworkz <akshay.saini@pyraworkz.in>
2021-07-10 15:00:26 +0530

    homepage changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
+​import { FilterPipe } from './filter.pipe';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -18,11 +20,16 @@ import { AccountsComponent } from './accounts/accounts.component';
     OrderSummaryComponent,
     AddressComponent,
     ProductDetailsComponent,
-    AccountsComponent
+    AccountsComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule ,
+    HttpClientModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+
+productList:any[]=[]
+productList1:any[]=[]
+cartItems:any[]=[];
+searchText:any;
+
+  constructor(private http:HttpClient) { 
+
+  }
 
   ngOnInit(): void {
+   this.getProductList();
   }
+  getProductList(){
+    this.http.get('http://139.59.6.161:3001/users/listProducts').subscribe(response=>{
+         console.log('ProductResponse',response)
+         let list=response['data'].map((data,index)=>{
+      if(index==0){
+      var data= data.kids.map(data=>{
+          data['category']=4;
+          return data;
+        })
+      }
+      else if(index==1){
+        var data= data.womens.map(data=>{
+          data['category']=3;
+          return data;
+        })
+      }
+      else if(index==2){
+        var data= data.mens.map(data=>{
+          data['category']=2;
+          return data;
+        })
+      }
+      return data;
+
+         });
+         console.log(list);
+         this.productList=[...list[0],...list[1],...list[2]]
+         console.log(this.productList);
+    },
+    err=>{
+
+    })
+  }
+
+  categoryFilter(value){
+     this.getProductList();
+     console.log('filter',value);
+     if(value>1){
+      this.productList= this.productList.filter(data=>{
+        return data.category==value;
+      });
+     }
+  }
+
+  addCart(item){
+    if(!this.cartItems.includes(item))
+    this.cartItems.push(item);
+    console.log(this.cartItems);
+    localStorage.setItem('cartItems',JSON.stringify(this.cartItems)
+    )
+    
+  }
+
+  
 
 }
